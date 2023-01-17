1e27de79fd86bcfa902193fdcf380594a2385952
AhmedA <alkhatib_ahmed1995@hotmail.com>
2023-01-17 16:49:34 -0500

    styled to match design, implemented local storage and update functionality

--- src/js/script.js
+++ src/js/script.js
@@ -10,25 +10,44 @@
 // -> list item is removed the list ✅
 // -> remove list item from allTodos array ✅
 
-const form = document.querySelector('.create-bar-form');
-const userInput = document.querySelector('.create-bar');
-const list = document.querySelector('.list');
-const deleteButtons = document.querySelectorAll('.delete-btn');
+// When user adds a task to the list
+// -> That task is added to local storage ✅
+// -> Task stays on list even when user leaves and returns to app ✅
+
+// When user updates a todo
+// -> When a task is clicked
+// -> User can update that task by entering a new one
+
+const form = document.querySelector(".create-bar-form");
+const userInput = document.querySelector(".create-bar");
+const list = document.querySelector(".list");
+const deleteButtons = document.querySelectorAll(".delete-btn");
 
 const state = {
   allTodos: [],
 };
 
 // GET TODOS FROM LOCAL STORAGE
+const savedTasksString = localStorage.getItem("taskListItem");
+const parsedTasks = JSON.parse(savedTasksString);
+state.allTodos = parsedTasks;
+
+state.allTodos.forEach((todo) => {
+  const li = document.createElement("li");
+  li.className = "list-item";
+  li.innerHTML = renderListItem(todo.text);
+  li.dataset.id = todo.id;
+  list.append(li);
+});
 
 // ADD TODO
 if (form instanceof HTMLFormElement) {
-  form?.addEventListener('submit', (e) => {
+  form?.addEventListener("submit", (e) => {
     e.preventDefault();
 
     if (userInput instanceof HTMLInputElement) {
-      const li = document.createElement('li');
-      li.className = 'list-item';
+      const li = document.createElement("li");
+      li.className = "list-item";
       li.innerHTML = renderListItem(userInput.value);
 
       const newTodo = {
@@ -39,17 +58,20 @@ if (form instanceof HTMLFormElement) {
       li.dataset.id = newTodo.id;
 
       list?.append(li);
-      userInput.value = '';
+      userInput.value = "";
 
       state.allTodos.push(newTodo);
+
+      // Save to local storage
+      localStorage.setItem("taskListItem", JSON.stringify(state.allTodos));
     }
   });
 }
 
 // REMOVE TODO
-list.addEventListener('click', (e) => {
+list.addEventListener("click", (e) => {
   const target = e.target;
-  if (target.classList.contains('d')) {
+  if (target.classList.contains("d")) {
     const deleteBtn = target.parentElement;
     const listItem = deleteBtn.parentElement;
     const listItemId = listItem.dataset.id;
@@ -65,6 +87,21 @@ list.addEventListener('click', (e) => {
 });
 
 // UPDATE TODO
+list.addEventListener("focusout", (e) => {
+  const target = e.target;
+  if (target.classList.contains("list-item-text") === true) {
+    const targetText = target.textContent;
+    const listItem = target.parentElement;
+    const listItemId = listItem.dataset.id;
+
+    for (var i = 0; i < state.allTodos.length; i++) {
+      if (state.allTodos[i].id === listItemId) {
+        state.allTodos[i].text = targetText;
+        localStorage.setItem("taskListItem", JSON.stringify(state.allTodos));
+      }
+    }
+  }
+});
 
 // VIEW FUNCTIONS
 /**
