e7875d2eca4312ab48d7378288727d10f864c9c0
AhmedA <alkhatib_ahmed1995@hotmail.com>
2023-01-17 17:21:38 -0500

    refactored code, added functions for create element and save to local storage

--- src/js/script.js
+++ src/js/script.js
@@ -15,8 +15,8 @@
 // -> Task stays on list even when user leaves and returns to app ✅
 
 // When user updates a todo
-// -> When a task is clicked
-// -> User can update that task by entering a new one
+// -> When a task is clicked ✅
+// -> User can update that task by entering a new one ✅
 
 const form = document.querySelector(".create-bar-form");
 const userInput = document.querySelector(".create-bar");
@@ -29,16 +29,17 @@ const state = {
 
 // GET TODOS FROM LOCAL STORAGE
 const savedTasksString = localStorage.getItem("taskListItem");
-const parsedTasks = JSON.parse(savedTasksString);
-state.allTodos = parsedTasks;
+if (savedTasksString === null) {
+  state.allTodos = [];
+} else {
+  const parsedTasks = JSON.parse(savedTasksString);
+  state.allTodos = parsedTasks;
 
-state.allTodos.forEach((todo) => {
-  const li = document.createElement("li");
-  li.className = "list-item";
-  li.innerHTML = renderListItem(todo.text);
-  li.dataset.id = todo.id;
-  list.append(li);
-});
+  state.allTodos.forEach((todo) => {
+    const li = createLi(todo.id, todo.text);
+    list.append(li);
+  });
+}
 
 // ADD TODO
 if (form instanceof HTMLFormElement) {
@@ -46,16 +47,12 @@ if (form instanceof HTMLFormElement) {
     e.preventDefault();
 
     if (userInput instanceof HTMLInputElement) {
-      const li = document.createElement("li");
-      li.className = "list-item";
-      li.innerHTML = renderListItem(userInput.value);
-
       const newTodo = {
         id: generateId(),
         text: userInput.value,
       };
 
-      li.dataset.id = newTodo.id;
+      const li = createLi(newTodo.id, newTodo.text);
 
       list?.append(li);
       userInput.value = "";
@@ -63,7 +60,7 @@ if (form instanceof HTMLFormElement) {
       state.allTodos.push(newTodo);
 
       // Save to local storage
-      localStorage.setItem("taskListItem", JSON.stringify(state.allTodos));
+      saveItem();
     }
   });
 }
@@ -83,6 +80,8 @@ list.addEventListener("click", (e) => {
     });
 
     state.allTodos = clickedTodo;
+
+    saveItem();
   }
 });
 
@@ -97,7 +96,7 @@ list.addEventListener("focusout", (e) => {
     for (var i = 0; i < state.allTodos.length; i++) {
       if (state.allTodos[i].id === listItemId) {
         state.allTodos[i].text = targetText;
-        localStorage.setItem("taskListItem", JSON.stringify(state.allTodos));
+        saveItem();
       }
     }
   }
@@ -123,7 +122,25 @@ function renderListItem(text) {
   return listItem;
 }
 
+/**
+ * Create a list element
+ * @param {string} id - This should be the id of the specific task on the list
+ * @param {string} text - This is the task
+ */
+
+function createLi(id, text) {
+  const li = document.createElement("li");
+  li.className = "list-item";
+  li.innerHTML = renderListItem(text);
+  li.dataset.id = id;
+  return li;
+}
+
 // HELPER FUNCTIONS
 function generateId() {
   return Math.random().toString(16).slice(2);
+}
+
+function saveItem() {
+  localStorage.setItem("taskListItem", JSON.stringify(state.allTodos));
 }
